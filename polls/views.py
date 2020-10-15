89dcb63a734e9db24052296a68f682818ff2361d
ervinismu <social.ervin@gmail.com>
2020-10-15 10:21:49 +0700

    Add static assets

--- polls/views.py
+++ polls/views.py
@@ -17,13 +17,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
